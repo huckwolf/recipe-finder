68f75be5090fe3538f150d5eac9453603fd581f6
root <root@huck-pc.(none)>
2014-05-11 14:04:21 +1000

    init unit test

---
+++ test/lib/Fridge/FridgeTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: PC
+ * Date: 14-5-11
+ * Time: 1:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Fridge;
+
+class FridgeTest{
+
+}
---
+++ test/src/RecipeFinderTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: PC
+ * Date: 14-5-11
+ * Time: 1:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace RecipeFinder;
+
+class RecipeFinderTest{
+
+}
